a286ab63b238a1ec1c780d18c5f9a0b551b31944
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2019-10-23 22:02:50 +0500

    initial commit

---
+++ AuctionIt/Models/ASPIdentity/ApplicationUser.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuctionIt.ASPNetIdentity
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("connectionString", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+    public class UserManager : UserManager<ApplicationUser>
+    {
+        public UserManager() : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
+        {
+            var dataProtectionProvider = Startup.DataProtectionProvider;
+            UserTokenProvider =
+                    new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ZATApp"));
+        }
+    }
+}
---
+++ AuctionIt/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.DataProtection;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuctionIt.Startup))]
+namespace AuctionIt
+{
+    public partial class Startup
+    {
+        internal static IDataProtectionProvider DataProtectionProvider { get; private set; }
+        public void Configuration(IAppBuilder app)
+        {
+            DataProtectionProvider = app.GetDataProtectionProvider();
+            ConfigureAuth(app);
+        }
+    }
+}
