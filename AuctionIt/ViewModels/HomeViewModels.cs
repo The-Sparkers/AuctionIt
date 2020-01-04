4617226ba24dfd90266cb5a8f5580a75f66d5b6c
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2020-01-04 22:34:45 +0500

    integrated layout with views

---
+++ AuctionIt/ViewModels/HomeViewModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AuctionIt.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IndexViewModel()
+        {
+            CurrentAuctions = new List<AuctionItemViewModel>(4);
+            FinishedAuctions = new List<FinishedAuctionViewModel>(4);
+        }
+        public List<AuctionItemViewModel> CurrentAuctions { get; set; }
+        public List<FinishedAuctionViewModel> FinishedAuctions { get; set; }
+    }
+
+    public class FinishedAuctionViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+    }
+    public class AuctionItemViewModel
+    {
+        public long Id { get; set; }
+        public TimeSpan TimeToEnd { get; set; }
+        public string Image { get; set; }
+        public decimal ActualPrice { get; set; }
+        public decimal HighestBid { get; set; }
+        public string ItemName { get; set; }
+        public int NumberOfBids { get; set; }
+    }
+}
