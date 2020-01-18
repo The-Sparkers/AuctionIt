59df2cd8dcd4f08469222683fbf14980142474a5
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2020-01-18 21:49:56 +0500

    code refactored

--- AuctionIt/App_Start/RouteConfig.cs
+++ AuctionIt/App_Start/RouteConfig.cs
@@ -10,11 +10,6 @@ namespace AuctionIt
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             
             routes.MapRoute(
-                name: "Pager",
-                url: "{controller}/{action}/{page}",
-                defaults: new { controller = "Home", action = "Index", page = 1 }
-            );
-            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
